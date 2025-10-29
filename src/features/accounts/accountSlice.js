5acde123fb4ba006383d1fb458ffbbecaf427f0e
VIPUL070 <sutharvipul007@gmail.com>
2025-10-29 20:47:48 +0530

    integrating redux toolkit and thunks middleware for better optimization

--- src/features/accounts/accountSlice.js
+++ src/features/accounts/accountSlice.js
@@ -1,55 +1,140 @@
-const initialStateAccount = 
+import { createSlice } from "@reduxjs/toolkit";
+//create action creator , make writing easier reducer , mutates states inside reducer
+
+const initialState  = 
 {
     balance : 0,
     loan : 0,
     loanPurpose : "",
+    isLoading : false,
 }
 
-export function accountReducer(state = initialStateAccount , action){
-  switch (action.type) {
-    case "account/deposit":
-        return {
-            ...state,
-            balance : state.balance + action.payload
-        }
+const accountSlice = createSlice({
+    name : 'account',
+    initialState,
+    reducers : {
+        deposit(state,action){
+           state.balance += action.payload;
+           state.isLoading = false;
+        },
+        withdraw(state,action){
+            state.balance -= action.payload;
+        },
 
-    case "account/withdraw":
-        return {
-            ...state,
-            balance : state.balance - action.payload
+        // to get more than 1 arg in payload
+        requestLoan :{
+            prepare(amount,purpose){
+                return {
+                    payload : {amount,purpose}
+                };
+            },
+        reducer(state,action){
+          if(state.loan > 0) return;
+          state.loan = action.payload.amount;
+          state.loanPurpose = action.payload.purpose;
+          state.balance += action.payload.amount;
+        }},
+
+        payLoan(state){
+            state.balance -= state.loan;
+            state.loan = 0;
+            state.loanPurpose = "";
+        },
+        convertingCurrency(state){
+          state.isLoading = true;
         }
+    }
+});
+
+export const {withdraw,requestLoan,payLoan} = accountSlice.actions;
+
+export function deposit(amount , currency) {
+    if(currency === 'USD')return {type : "account/deposit" , payload : amount}
     
-    case "account/requestLoan":
-        if(state.loan > 0) return state;
-        return {
-           ...state,
-           loan : action.payload.amount,
-           loanPurpose : action.payload.purpose,
-           balance : state.balance + action.payload.amount
-        }
+    // return function means this is async function executed after dispatch and before reducer updation
+    return async function (dispatch) {
+       dispatch({type : "account/convertingCurrency"});
+
+      //API call
+      const res = await fetch(`https://api.frankfurter.dev/v1/latest?amount=${amount}}&from=${currency}&to=USD`);
+      const data = await res.json();
+      const convertedCurrency = data.rates.USD;
+
+      //dispatch action
+      dispatch({ type : "account/deposit" , payload : convertedCurrency})
+    }
+}
+
+export default accountSlice.reducer;
+
+//older version
+// export function accountReducer(state = initialStateAccount , action){
+//   switch (action.type) {
+//     case "account/deposit":
+//         return {
+//             ...state,
+//             balance : state.balance + action.payload,
+//             isLoading : false,
+//         }
+
+//     case "account/withdraw":
+//         return {
+//             ...state,
+//             balance : state.balance - action.payload
+//         }
+    
+//     case "account/requestLoan":
+//         if(state.loan > 0) return state;
+//         return {
+//            ...state,
+//            loan : action.payload.amount,
+//            loanPurpose : action.payload.purpose,
+//            balance : state.balance + action.payload.amount
+//         }
      
-    case "account/payLoan":
-        return {
-            ...state,
-            balance : state.balance - state.loan,
-            loan : 0,
-            loanPurpose : ""
-        }
+//     case "account/payLoan":
+//         return {
+//             ...state,
+//             balance : state.balance - state.loan,
+//             loan : 0,
+//             loanPurpose : ""
+//         }
     
-    default:
-        return state;
-  }
-}
+//     case "account/convertingCurrency" :
+//         return {
+//             ...state,
+//             isLoading : true,
+//         }
+    
+//     default:
+//         return state;
+//   }
+// }
 
-export function deposit(amount) {
-    return {type : "account/deposit" , payload : amount}
-}
-export function withdraw(amount) {
-    return {type : "account/withdraw" , payload : amount}
-}
-export function requestLoan(amount ,purpose) {
-    return {type : "account/requestLoan" , payload : {amount , purpose}}
-}
-export function payLoan() {
-    return {type : "account/payLoan"}
-}
+// export function deposit(amount , currency) {
+//     if(currency === 'USD')return {type : "account/deposit" , payload : amount}
+    
+//     // return function means this is async function executed after dispatch and before reducer updation
+//     return async function (dispatch , getState) {
+//        dispatch({type : "account/convertingCurrency"});
+
+//       //API call
+//       const res = await fetch(`https://api.frankfurter.dev/v1/latest?amount=${amount}}&from=${currency}&to=USD`);
+//       const data = await res.json();
+//       const convertedCurrency = data.rates.USD;
+
+//       //dispatch action
+//       dispatch({ type : "account/deposit" , payload : convertedCurrency})
+//     }
+// }
+
+// //action creators
+// export function withdraw(amount) {
+//     return {type : "account/withdraw" , payload : amount}
+// }
+// export function requestLoan(amount ,purpose) {
+//     return {type : "account/requestLoan" , payload : {amount , purpose}}
+// }
+// export function payLoan() {
+//     return {type : "account/payLoan"}
+// }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {Provider} from 'react-redux'
 import './index.css'
 import App from './App.jsx'
 import store from './store.js'
-store.dispatch({type : "account/deposit" , payload : 500});
-console.log(store.getState());
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
